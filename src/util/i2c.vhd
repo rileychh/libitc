a47979ee0d10c24253c343af0fb42cb6e0d28acb
Chengxuan <53939195+WeedForJustice@users.noreply.github.com>
2020-10-09 16:56:37 +0800

    Updata i2c (not tested)

--- src/util/i2c.vhd
+++ src/util/i2c.vhd
@@ -1,12 +1,62 @@
--- I2C (IIC) master driver
--- based on https://www.digikey.com/eewiki/pages/viewpage.action?pageId=10125324
+-- I2C (IIC) master interface
+--
+-- reference:
+-- https://www.digikey.com/eewiki/pages/viewpage.action?pageId=10125324
+-- https://www.youtube.com/watch?v=skkyudHHSWY
+--
+-- usage:
+-- 1. to write a byte: 
+--    addr = slave address
+--    rw = '0'
+--    data_tx = byte to write
+--    ena = high pulse
+--    wait for falling edge of busy
+--
+-- 2. to read a byte: 
+--    addr = slave address
+--    rw = '1'
+--    ena = high pulse
+--    wait for falling edge of busy
+--    your data = data_rx
+--
+-- 3. to write to a register: 
+--    addr = slave address
+--    rw = '0'
+--    data_tx = register address
+--    ena = '1'
+--    wait for falling edge of busy
+--    data_tx = 1st byte to write
+--    wait for falling edge of busy
+--    data_tx = 2nd byte to write
+--    ...
+--    ena = '0'
+--
+-- 4. to read a register: 
+--    addr = slave address
+--    rw = '0'
+--    data_tx = register address
+--    ena = '1'
+--    wait for falling edge of busy
+--    1st byte read = data_rx
+--    wait for falling edge of busy
+--    2nd byte read = data_rx
+--    ...
+--    ena = '0'
+--
+
+library ieee;
+use ieee.std_logic_1164.all;
+use ieee.numeric_std.all;
+
+package i2c_p is
+
+end package;
 
 library ieee;
 use ieee.std_logic_1164.all;
 use ieee.numeric_std.all;
 
 use work.i2c_p.all;
-use work.clk_p.all;
 
 entity i2c is
 	port (
@@ -16,27 +66,108 @@ entity i2c is
 		-- internal
 		clk     : in std_logic;             -- 400kHz
 		rst     : in std_logic;             -- low active
+		ena     : in std_logic;             -- if high, latch in new input
+		busy    : out std_logic;            -- if high, addr, rw and data_tx will be ignored
 		addr    : in unsigned(6 downto 0);  -- slave address
 		rw      : in std_logic;             -- high read, low write
-		data_tx : in unsigned(7 downto 0);  -- byte to write to slave
 		data_rx : out unsigned(7 downto 0); -- byte read from slave
-		busy    : out std_logic             -- if high; addr, rw and data_tx will be ignored
+		data_tx : in unsigned(7 downto 0)   -- byte to write to slave
 	);
 end i2c;
 
 architecture arch of i2c is
 
+	-- I2C state machine
+	-- | state | read/write | what                                    |
+	-- | ----- | ---------- | --------------------------------------- |
+	-- | idle  | -          | do nothing                              |
+	-- | start | w          | send start condition                    |
+	-- | cmd   | w          | send 6-bit address + 1-bit read / write |
+	-- | ack1  | r          | receive slave acknowledgment bit        |
+	-- | data  | r/w        | receive/send data byte                  |
+	-- | ack2  | w/r        | send/receive acknowledgment bit         |
+	-- | stop  | w          | send stop condition                     |
+	type state_t is (idle, start, cmd, ack1, data, ack2, stop);
+	signal state : state_t;
+
+	-- SCL and SDA wire: to change final output from '0' and '1' to '0' and 'X';
+	signal scl_wire, sda_wire : std_logic;
+
+	-- input latches: save input on rising edge of start
+	signal cmd_reg : unsigned(7 downto 0); -- command = addr + rw
+	signal data_tx_reg : unsigned(7 downto 0);
+
+	-- procedure to latch in new input value
+	procedure update(
+		addr        : in unsigned(6 downto 0);
+		rw          : in std_logic;
+		data_tx     : in unsigned(7 downto 0);
+		cmd_reg     : out unsigned(7 downto 0);
+		data_tx_reg : out unsigned(7 downto 0)
+	) is begin
+		cmd_reg <= addr & rw;
+		data_tx_reg <= data_tx;
+	end procedure;
+
+	-- SCL enable: release SCL when resetting, idling, starting or stopping
 	signal scl_ena : std_logic;
+
+	-- bit count: loop inside state for a byte
+	signal cnt : integer range 0 to 7;
 
 begin
 
+	busy <= '1' when state /= idle or rst = '0' else '0';
+
+	-- state machine
+	process (clk, rst) begin
+		if rst = '0' then
+			state <= idle;
+		elsif falling_edge(clk) then
+			case state is
+				when idle =>
+					if ena = '1' then
+						update(addr, rw, data_tx, cmd_reg, data_tx_reg);
+						state <= start;
+					end if;
+				when start =>
+					state <= cmd;
+					cnt <= 7; -- prepare cnt for command state
+				when cmd =>
+					if cnt = 0 then
+						state <= ack1;
+					end if;
+					cnt <= cnt - 1;
+				when ack1 =>
+					state <= data;
+					cnt <= 7; -- prepare cnt for data state	
+				when data =>
+					if cnt = 0 then
+						state <= ack2;
+					end if;
+					cnt <= cnt - 1;
+				when ack2 =>
+					if ena = '1' then -- continuous mode
+						update(addr, rw, data_tx, cmd_reg, data_tx_reg);
+						if cmd_reg = addr & rw then
+							state <= data; -- keep sending/receiving bytes
+						else
+							state <= start; -- send a restart
+						end if;
+					else
+						state <= stop;
+					end if;
+				when stop =>
+					state <= idle;
+			end case;
+		end if;
+	end process;
+
 	-- SCL control
-	-- falling edge and inverting the clock is used to make scl and sda out of
-	-- phase, thus complying to I2C protocol
 	process (clk, rst) begin
 		if rst = '0' then
 			scl_ena <= '0';
-		elsif falling_edge(clk) then
+		elsif rising_edge(clk) then
 			case state is
 				when idle | start | stop => scl_ena <= '0';
 				when others => scl_ena <= '1';
@@ -44,60 +175,57 @@ begin
 		end if;
 	end process;
 
-	scl <= not clk when scl_ena = '1' else '1';
+	scl_wire <= clk when scl_ena = '1' else '1';
 
-	-- SDA and state control (main state machine)
-	process (clk, rst)
-
-		type state_t is (idle, start, addr, rw, ack1, data, ack2, stop);
-		variable state : state_t;
-
-		variable cnt : integer range 0 to 7;
-
-	begin
-
-		if rst = '0' then
-			state <= idle;
-		elsif rising_edge(clk) then
+	-- SDA write control
+	process (clk) begin
+		if falling_edge(clk) then -- write when SCL is low
 			case state is
 				when idle =>
-					sda <= '1';
-					state <= start;
-				when start =>
-					sda <= '0'; -- scl = '1' and falling_edge(sda) == start
-					state <= addr;
-					busy <= '1';
-					cnt := 6; -- prepare cnt for address state
-				when addr => -- send 7-bit address, MSB first
-					sda <= addr(cnt);
-					if cnt = 0 then
-						state <= rw;
-					else
-						cnt := cnt - 1;
-					end if;
-				when rw => -- send read / write bit
-					sda <= rw;
-					state <= ack1;
-				when ack1 => -- receive acknowledgment bit
-					-- TODO handle no acknowledgment, currently ignored
-					state <= data;
-					cnt := 7; -- prepare cnt for data state	
+					sda_wire <= '1';
+				when cmd => -- send 7-bit address plus 1-bit read/write, MSB first
+					sda_wire <= cmd_reg(cnt);
 				when data =>
-					sda <= data(cnt);
-					if cnt = 0 then
-						state <= ack2;
-					else
-						cnt := cnt - 1;
+					if cmd_reg(0) = '0' then -- r/w bit is write
+						sda_wire <= data_tx_reg(cnt);
+					end if;
+				when ack2 =>
+					if cmd_reg(0) = '1' then -- r/w bit is read
+						sda_wire <= '0'; -- send acknowledgment bit
+					end if;
+				when stop =>
+					-- TODO should sda be '0' first?
+				when others => null;
+			end case;
+		end if;
+	end process;
+
+	-- I2C start/stop and SDA read control
+	process (clk) begin
+		if rising_edge(clk) then -- write start/stop or read when SCL is high
+			case state is
+				when start =>
+					sda_wire <= '0'; -- scl = '1' and falling_edge(sda) == start
+				when ack1 =>
+					-- TODO handle no acknowledgment, currently ignored
+				when data =>
+					if cmd_reg(0) = '1' then -- r/w bit is read
+						data_rx_reg(cnt) <= sda_wire; -- cnt is controlled by write process
 					end if;
 				when ack2 =>
 					-- TODO handle no acknowledgment, currently ignored
-					state <= stop;
+					if cmd_reg(0) = '0' then -- r/w bit is write
+					end if;
 				when stop =>
-					sda <= '1'; -- scl = '1' and rising_edge(sda) == stop
-					state <= idle;
+					sda_wire <= '1'; -- scl = '1' and rising_edge(sda) == stop
+				when others => null;
 			end case;
 		end if;
-
 	end process;
+
+	-- convert internal wire to open drain
+	-- TODO should it include a pull-up ('H') or just high-z ('Z')?
+	scl <= 'H' when scl_wire = '1' else '0';
+	sda <= 'H' when sda_wire = '1' else '0';
 
 end arch;
