8a9c5015f7f637d09c67c2270620fae7d31e325e
Chengxuan <53939195+WeedForJustice@users.noreply.github.com>
2020-10-09 22:50:10 +0800

    Add TSL2561 test

---
+++ tests/tsl_seg_test.vhd
@@ -0,0 +1,15 @@
+library ieee;
+use ieee.std_logic_1164.all;
+use ieee.numeric_std.all;
+
+entity tsl_test is
+	port (
+		
+	);
+end tsl_test;
+
+architecture arch of tsl_test is
+	
+begin
+	
+end arch;
