22799c42c7592ba6f4d0b0f14f341b22c1f26b3f
Chengxuan <53939195+WeedForJustice@users.noreply.github.com>
2020-10-24 17:15:05 +0800

    i2c rewrite stage 1

--- src/tsl.vhd
+++ src/tsl.vhd
@@ -11,7 +11,12 @@ package tsl_p is
 			clk : in std_logic;
 			rst : in std_logic;
 			-- user logic
-			lux : out integer range 0 to 40000 -- calculated illuminance from sensors
+			lux : out integer range 0 to 40000; -- calculated illuminance from sensors
+			-- debug
+			dbg_state : out integer range 0 to 8;
+			tsl_state : out integer range 0 to 3;
+			dbg_reg   : out integer range 0 to 3;
+			dbg_cnt   : out integer range 0 to 8
 		);
 	end component;
 end package;
@@ -31,7 +36,12 @@ entity tsl is
 		clk : in std_logic;
 		rst : in std_logic;
 		-- user logic
-		lux : out integer range 0 to 40000 -- calculated illuminance from sensors
+		lux : out integer range 0 to 40000; -- calculated illuminance from sensors
+		-- debug
+		dbg_state : out integer range 0 to 8;
+		tsl_state : out integer range 0 to 3;
+		dbg_reg   : out integer range 0 to 3;
+		dbg_cnt   : out integer range 0 to 8
 	);
 end tsl;
 
@@ -50,24 +60,24 @@ architecture arch of tsl is
 	constant reg_data_0 : unsigned(7 downto 0) := x"ac"; -- (read) word from data register 0
 	constant reg_data_1 : unsigned(7 downto 0) := x"ae"; -- (read) word from data register 1
 
-	-- state machines
-	type tsl_state_t is (init, read_data_0, read_data_1);
-	signal state : tsl_state_t;
-	type i2c_step_t is (sel_reg, byte_0, byte_1);
-	signal step : i2c_step_t;
+	-- state machine
+	type i2c_state_t is (reg, data);
+	signal state : i2c_state_t;
+
+	-- init register: turn power on only once
+	signal init : std_logic;
 
 	-- I2C wires/registers
-	signal i2c_clk : std_logic;
 	signal i2c_ena : std_logic;
 	signal i2c_busy : std_logic;
 	signal i2c_rw : std_logic;
-	signal i2c_rx : unsigned(7 downto 0);
-	signal i2c_tx : unsigned(7 downto 0);
-
-	signal i2c_busy_prev : std_logic; -- register to store busy value from last clock
-	signal i2c_done : std_logic; -- register to indicate transmission is done
+	signal i2c_in : unsigned(7 downto 0);
+	signal i2c_out : unsigned(7 downto 0);
+	signal i2c_accepted : std_logic; -- indicates interface accepted data
+	signal i2c_done : std_logic; -- indicates transmission is done
 
 	-- sensor values
+	signal reg_sel : integer range 0 to 3; -- select between ADC 0 and ADC 1
 	signal data_0 : unsigned(15 downto 0);
 	signal data_1 : unsigned(15 downto 0);
 
@@ -120,115 +130,111 @@ architecture arch of tsl is
 
 begin
 
-	clk_inst : entity work.clk(arch)
-		generic map(
-			freq => 10
-		)
-		port map(
-			clk_in => clk,
-			rst => rst,
-			clk_out => i2c_clk
-		);
+	tsl_state <= i2c_state_t'pos(state) when init = '0' else i2c_state_t'pos(state) + 2;
+	dbg_reg <= reg_sel;
 
 	i2c_inst : entity work.i2c(arch)
+		generic map(
+			bus_freq => 1
+		)
 		port map(
-			scl  => tsl_scl,
-			sda  => tsl_sda,
-			clk  => i2c_clk,
-			rst  => rst,
-			ena  => i2c_ena,
-			busy => i2c_busy,
-			addr => tsl_addr,
-			rw   => i2c_rw,
-			rx   => i2c_rx,
-			tx   => i2c_tx
+			scl       => tsl_scl,
+			sda       => tsl_sda,
+			clk       => clk,
+			rst       => rst,
+			ena       => i2c_ena,
+			busy      => i2c_busy,
+			addr      => tsl_addr,
+			rw        => i2c_rw,
+			data_in   => i2c_in,
+			data_out  => i2c_out,
+			dbg_state => dbg_state,
+			dbg_cnt   => dbg_cnt
 		);
 
-	i2c_done <= i2c_busy_prev and not i2c_busy;
+	edge_inst : entity work.edge(arch) -- detect falling edge of i2c_busy
+		port map(
+			clk       => clk,
+			rst       => rst,
+			signal_in => i2c_busy,
+			rising    => i2c_accepted,
+			falling   => i2c_done
+		);
 
 	process (clk, rst) begin
 		if rst = '0' then
 			lux <= 0;
-			state <= init;
-			step <= sel_reg;
+			init <= '0';
+			state <= reg;
+			reg_sel <= 0; -- read the first byte of data
 		elsif rising_edge(clk) then
-			i2c_busy_prev <= i2c_busy; -- update I2C busy flag
+			if init = '0' then -- turn power on (setup)
+				case state is
+					when reg => -- select control register
+						i2c_rw <= '0'; -- write
+						i2c_in <= reg_ctrl;
+						i2c_ena <= '1'; -- start transmission
 
-			case state is
-				when init => -- turn power on
-					case step is
-						when sel_reg => -- select control register
-							i2c_rw <= '0'; -- write
-							i2c_tx <= reg_ctrl;
-							i2c_ena <= '1'; -- start transmission
+						if i2c_done = '1' then -- register byte done
+							i2c_in <= ctrl_power_on; -- continue with another write
+							state <= data; -- next step
+						end if;
 
-							if i2c_done = '1' then
-								step <= byte_0; -- next step
-							end if;
-						when byte_0 => -- write power on to control register
-							i2c_tx <= ctrl_power_on;
+					when data => -- write power on to control register
+						if i2c_accepted = '1' then -- register byte accepted
+							i2c_ena <= '0'; -- this is the last byte
+						end if;
 
-							if i2c_done = '1' then
-								i2c_ena <= '0'; -- stop transmission
-								step <= sel_reg; -- reset step
-								state <= read_data_0; -- next state
-							end if;
-						when others => null; -- there's no second byte
-					end case;
+						if i2c_done = '1' then
+							state <= reg; -- reset step
+							init <= '1'; -- don't run init again
+						end if;
+				end case;
+			else -- read the ADC channels' value (loop)
+				case state is
+					when reg => -- select data register
+						i2c_rw <= '0'; -- write
+						case reg_sel is
+							when 0 | 1 =>
+								i2c_in <= reg_data_0;
+							when 2 | 3 =>
+								i2c_in <= reg_data_1;
+						end case;
+						i2c_ena <= '1';
 
-				when read_data_0 => -- read the ADC channel 0 value
-					case step is
-						when sel_reg =>
-							i2c_rw <= '0'; -- write
-							i2c_tx <= reg_data_0;
-							i2c_ena <= '1';
-
-							if i2c_done = '1' then
-								step <= byte_0;
-							end if;
-						when byte_0 =>
+						if i2c_done = '1' then
 							i2c_rw <= '1'; -- read
+							state <= data;
+						end if;
 
-							if i2c_done = '1' then
-								data_0(7 downto 0) <= i2c_rx; -- lower byte
-								step <= byte_1;
-							end if;
-						when byte_1 =>
-							if i2c_done = '1' then
-								i2c_ena <= '0';
-								data_0(15 downto 8) <= i2c_rx; -- upper byte
-								step <= sel_reg;
-								state <= read_data_1; -- read ADC channel 1
-							end if;
-					end case;
+					when data =>
+						if i2c_accepted = '1' then -- register byte accepted
+							case reg_sel is
+								when 0 | 2 => -- upper byte
+									i2c_ena <= '1'; -- continue with another read
+								when 1 | 3 => -- lower byte
+									i2c_ena <= '0'; -- this is the last byte
+							end case;
+						end if;
 
-				when read_data_1 => -- read the ADC channel 1 value
-					case step is
-						when sel_reg =>
-							i2c_rw <= '0'; -- write
-							i2c_tx <= reg_data_1;
-							i2c_ena <= '1';
-
-							if i2c_done = '1' then
-								step <= byte_0;
-							end if;
-						when byte_0 =>
-							i2c_rw <= '0'; -- read
-
-							if i2c_done = '1' then
-								data_1(7 downto 0) <= i2c_rx; -- lower byte
-								step <= byte_1;
-							end if;
-						when byte_1 =>
-							if i2c_done = '1' then
-								i2c_ena <= '0';
-								data_1(15 downto 8) <= i2c_rx; -- upper byte
-								step <= sel_reg;
-								state <= read_data_0; -- back to reading ADC channel 0
-								lux <= to_lux(data_0, data_1);
-							end if;
-					end case;
-			end case;
+						if i2c_done = '1' then
+							case reg_sel is
+								when 0 => -- ADC 0 lower byte
+									data_0(7 downto 0) <= i2c_out;
+								when 1 => -- ADC 0 upper byte
+									data_0(15 downto 8) <= i2c_out;
+									state <= reg;
+								when 2 => -- ADC 1 lower byte
+									data_1(7 downto 0) <= i2c_out;
+								when 3 => -- ADC 1 upper byte
+									data_1(15 downto 8) <= i2c_out;
+									state <= reg;
+									lux <= to_lux(data_0, data_1);
+							end case;
+							reg_sel <= reg_sel + 1;
+						end if;
+				end case;
+			end if;
 		end if;
 	end process;
 end arch;
