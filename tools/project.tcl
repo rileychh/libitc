ee4b6cbe718e17054c542824c91b39ca1ff8350f
Chengxuan <53939195+WeedForJustice@users.noreply.github.com>
2020-10-26 00:20:43 +0800

    Change top entity to tts_test

--- tools/project.tcl
+++ tools/project.tcl
@@ -37,12 +37,13 @@ if {$make_assignments} {
 	set_global_assignment -name VHDL_INPUT_VERSION VHDL_2008
 	set_global_assignment -name SEARCH_PATH src\\util
 	set_global_assignment -name SEARCH_PATH src
-	set_global_assignment -name TOP_LEVEL_ENTITY tsl_seg_test
+	set_global_assignment -name TOP_LEVEL_ENTITY tts_test
 	set_global_assignment -name VHDL_FILE tests/key_seg_test.vhd
 	set_global_assignment -name VHDL_FILE tests/dot_test.vhd
 	set_global_assignment -name VHDL_FILE tests/bcd_seg_test.vhd
 	set_global_assignment -name VHDL_FILE tests/dht_seg_test.vhd
 	set_global_assignment -name VHDL_FILE tests/tsl_seg_test.vhd
+	set_global_assignment -name VHDL_FILE tests/tts_test.vhd
 	set_global_assignment -name VHDL_FILE src/seg.vhd
 	set_global_assignment -name VHDL_FILE src/key.vhd
 	set_global_assignment -name VHDL_FILE src/dht.vhd
