f0448c2ec93d9c4a9e8193c6d7fc76d735337671
Chengxuan <53939195+WeedForJustice@users.noreply.github.com>
2020-11-09 23:20:05 +0800

    Update dot_test

--- tests/dot_test.vhd
+++ tests/dot_test.vhd
@@ -8,6 +8,8 @@ entity dot_test is
 	port (
 		-- sys
 		clk, rst_n : in std_logic;
+		-- seg
+		seg_1, seg_2, seg_s : out byte_be_t; -- abcdefgp * 2, seg2_s1 ~ seg1_s4
 		-- dot
 		dot_r, dot_g, dot_s : out byte_be_t;
 		-- sw 
@@ -17,9 +19,10 @@ end dot_test;
 
 architecture arch of dot_test is
 
-	-- test buffers for dot
-	constant test_buf_r : bytes_be_t(0 to 7) := ("11111111", "10000001", "10111101", "10100101", "10100101", "10111101", "10000001", "11111111");
-	constant test_buf_g : bytes_be_t(0 to 7) := ("11111111", "11111111", "11000011", "11011011", "11011011", "11000011", "11111111", "11111111");
+	signal dot_data_r, dot_data_g : bytes_be_t(0 to 7);
+
+	signal x_pos, y_pos : integer range 0 to 7;
+	signal clk_pos : std_logic;
 
 begin
 
@@ -30,8 +33,58 @@ begin
 			dot_r  => dot_r,
 			dot_g  => dot_g,
 			dot_s  => dot_s,
-			data_r => test_buf_r,
-			data_g => test_buf_g
+			data_r => dot_data_r,
+			data_g => dot_data_g
 		);
+
+	seg_inst : entity work.seg(arch)
+		port map(
+			clk   => clk,
+			rst_n => rst_n,
+			seg_1 => seg_1,
+			seg_2 => seg_2,
+			seg_s => seg_s,
+			data  => to_string(x_pos, x_pos'high, 10, 4) & to_string(y_pos, y_pos'high, 10, 4),
+			dot => (others => '0')
+		);
+
+	clk_inst : entity work.clk(arch)
+		generic map(
+			freq => 8
+		)
+		port map(
+			clk_in  => clk,
+			rst_n   => rst_n,
+			clk_out => clk_pos
+		);
+
+	process (clk_pos, rst_n) begin
+		if rst_n = '0' then
+			x_pos <= 0;
+			y_pos <= 0;
+		elsif rising_edge(clk_pos) then
+			if x_pos = x_pos'high then
+				x_pos <= 0;
+				if y_pos = y_pos'high then
+					y_pos <= 0;
+				else
+					y_pos <= y_pos + 1;
+				end if;
+			else
+				x_pos <= x_pos + 1;
+			end if;
+
+			dot_data_r <= (others => (others => '0'));
+			dot_data_g <= (others => (others => '0'));
+
+			if sw(0) = '1' then
+				dot_data_g(x_pos)(y_pos) <= '1';
+			end if;
+
+			if sw(1) = '1' then
+				dot_data_r(x_pos)(y_pos) <= '1';
+			end if;
+		end if;
+	end process;
 
 end arch;
