e3abe073c79d7aac77a21dcbe9b2141841f9d26e
Chenghsuan Ho <chengxuanjhe@gmail.com>
2023-08-24 01:37:10 +0800

    讓 genmif 可匯入做為函式庫

--- tools/gpu/genmif.py
+++ tools/gpu/genmif.py
@@ -1,13 +1,123 @@
 #!/usr/bin/env python
 
 from argparse import ArgumentParser, FileType
+from enum import Enum
+from io import BufferedIOBase, TextIOBase
 from typing import Union
 
 from PIL import Image
 from PIL.ImageColor import getrgb
 
 
-def load_args():
+def fill(im: Image.Image, size: tuple[int, int]) -> Image.Image:
+    aspect = im.width / im.height
+    new_aspect = size[0] / size[1]
+
+    if aspect > new_aspect:
+        # Then crop the left and right edges:
+        target_width = int(new_aspect * im.height)
+        offset = (im.width - target_width) / 2
+        new_box = (offset, 0, im.width - offset, im.height)
+    else:
+        # ... crop the top and bottom:
+        target_height = int(im.width / new_aspect)
+        offset = (im.height - target_height) / 2
+        new_box = (0, offset, im.width, im.height - offset)
+
+    return im.crop(new_box).resize(size)
+
+
+def fit(
+    im: Image.Image, size: tuple[int, int], fill_color: tuple[int, int, int]
+) -> Image.Image:
+    resized = im.copy()
+    resized.thumbnail(size)
+    res = Image.new("RGB", size, fill_color)
+    res.paste(
+        resized, ((size[0] - resized.width) // 2, (size[1] - resized.height) // 2)
+    )
+    return res
+
+
+def format_pixel(depth: int, pixel: Union[tuple[int, int, int], int]) -> str:
+    if depth == 1:
+        return "1" if pixel else "0"
+    elif depth == 3:
+        r, g, b = pixel
+        return "{:x}".format((r >> 7) << 2 | (g >> 7) << 1 | b >> 7)
+    elif depth == 8:
+        r, g, b = pixel
+        return "{:x}".format((r >> 5) << 5 | (g >> 5) << 2 | (b >> 6))
+    else:
+        r, g, b = pixel
+        return "{:x}".format(r << 16 | g << 8 | b)
+
+
+class CropMode(Enum):
+    NONE = 0
+    FILL = 1
+    FIT = 2
+
+    def __str__(self):
+        if self == CropMode.NONE:
+            return "in"
+        elif self == CropMode.FILL:
+            return "fills"
+        elif self == CropMode.FIT:
+            return "fits"
+
+    def from_str(mode: str):
+        if mode == "none":
+            return CropMode.NONE
+        elif mode == "fill":
+            return CropMode.FILL
+        elif mode == "fit":
+            return CropMode.FIT
+        else:
+            raise ValueError(f"Invalid mode: {mode}")
+
+
+def generate(
+    size: tuple[int, int],
+    depth: int,
+    crop: CropMode,
+    background: tuple[int, int, int],
+    image: BufferedIOBase,
+    mif: TextIOBase,
+):
+    im = Image.open(image).convert("RGB")
+
+    if crop == CropMode.FIT:
+        im = fit(im, size, background)
+    elif crop == CropMode.FILL:
+        im = fill(im, size)
+
+    if depth == 1:
+        im = im.convert("1")
+
+    pixels = list(im.getdata())
+
+    mif_header = f"""\
+WIDTH={str(depth)};
+DEPTH={len(pixels)};
+
+ADDRESS_RADIX=UNS;
+DATA_RADIX=HEX;
+
+CONTENT BEGIN
+"""
+
+    mif_footer = """\
+END;
+"""
+
+    mif_data = "".join(
+        f"\t{i}: {format_pixel(depth, p)};\n" for i, p in enumerate(pixels)
+    )
+    mif.write(mif_header + mif_data + mif_footer)
+
+
+if __name__ == "__main__":
     parser = ArgumentParser()
 
     parser.add_argument(
@@ -46,81 +156,14 @@ def load_args():
 
     if args.crop != "fit" and args.background is not None:
         parser.error("background color is only used when crop mode is fit")
-    elif args.crop == "fit" and args.background is None:
+    elif args.background is None:
         args.background = "#000"
-    return args
 
-
-def fill(im: Image.Image, size: tuple[int, int]) -> Image.Image:
-    aspect = im.width / im.height
-    new_aspect = size[0] / size[1]
-
-    if aspect > new_aspect:
-        # Then crop the left and right edges:
-        target_width = int(new_aspect * im.height)
-        offset = (im.width - target_width) / 2
-        new_box = (offset, 0, im.width - offset, im.height)
-    else:
-        # ... crop the top and bottom:
-        target_height = int(im.width / new_aspect)
-        offset = (im.height - target_height) / 2
-        new_box = (0, offset, im.width, im.height - offset)
-
-    return im.crop(new_box).resize(size)
-
-
-def fit(
-    im: Image.Image, size: tuple[int, int], fill_color: tuple[int, int, int]
-) -> Image.Image:
-    resized = im.copy()
-    resized.thumbnail(size)
-    res = Image.new("RGB", size, fill_color)
-    res.paste(
-        resized, ((size[0] - resized.width) // 2, (size[1] - resized.height) // 2)
+    generate(
+        size=args.size,
+        depth=args.depth,
+        crop=CropMode.from_str(args.crop),
+        background=getrgb(args.background),
+        image=args.image,
+        mif=args.mif,
     )
-    return res
-
-
-def format_pixel(pixel: Union[tuple[int, int, int], int]) -> str:
-    if args.depth == 1:
-        return "1" if pixel else "0"
-    elif args.depth == 3:
-        r, g, b = pixel
-        return "{:x}".format((r >> 7) << 2 | (g >> 7) << 1 | b >> 7)
-    elif args.depth == 8:
-        r, g, b = pixel
-        return "{:x}".format((r >> 5) << 5 | (g >> 5) << 2 | (b >> 6))
-    else:
-        r, g, b = pixel
-        return "{:x}".format(r << 16 | g << 8 | b)
-
-
-args = load_args()
-im = Image.open(args.image).convert("RGB")
-
-if args.crop == "fit":
-    im = fit(im, args.size, getrgb(args.background))
-elif args.crop == "fill":
-    im = fill(im, args.size)
-
-if args.depth == 1:
-    im = im.convert("1")
-
-pixels = list(im.getdata())
-
-mif_header = f"""\
-WIDTH={str(args.depth)};
-DEPTH={len(pixels)};
-
-ADDRESS_RADIX=UNS;
-DATA_RADIX=HEX;
-
-CONTENT BEGIN
-"""
-
-mif_footer = """\
-END;
-"""
-
-mif_data = "".join(f"\t{i}: {format_pixel(p)};\n" for i, p in enumerate(pixels))
-args.mif.write(mif_header + mif_data + mif_footer)
