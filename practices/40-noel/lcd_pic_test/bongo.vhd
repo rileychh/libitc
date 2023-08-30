6c9ec6715727c8ea1db2f0d5cc1aa3c75a8d3b46
MingHaoTai <nnn122nnn70@gmail.com>
2023-08-30 18:53:25 +0800

    lcd 圖片測試

--- practices/40-noel/lcd_pic_test/bongo.vhd
+++ practices/40-noel/lcd_pic_test/bongo.vhd
@@ -4,7 +4,7 @@
 -- MODULE: altsyncram 
 
 -- ============================================================
--- File Name: bongo2.vhd
+-- File Name: bongo.vhd
 -- Megafunction Name(s):
 -- 			altsyncram
 --
@@ -39,17 +39,17 @@ USE ieee.std_logic_1164.all;
 LIBRARY altera_mf;
 USE altera_mf.altera_mf_components.all;
 
-ENTITY bongo2 IS
+ENTITY bongo IS
 	PORT
 	(
-		address		: IN STD_LOGIC_VECTOR (14 DOWNTO 0);
+		address		: IN STD_LOGIC_VECTOR (7 DOWNTO 0);
 		clock		: IN STD_LOGIC  := '1';
 		q		: OUT STD_LOGIC_VECTOR (23 DOWNTO 0)
 	);
-END bongo2;
+END bongo;
 
 
-ARCHITECTURE SYN OF bongo2 IS
+ARCHITECTURE SYN OF bongo IS
 
 	SIGNAL sub_wire0	: STD_LOGIC_VECTOR (23 DOWNTO 0);
 
@@ -61,15 +61,15 @@ BEGIN
 		address_aclr_a => "NONE",
 		clock_enable_input_a => "BYPASS",
 		clock_enable_output_a => "BYPASS",
-		init_file => "bongo2.mif",
+		init_file => "bongo.mif",
 		intended_device_family => "Cyclone III",
 		lpm_hint => "ENABLE_RUNTIME_MOD=NO",
 		lpm_type => "altsyncram",
-		numwords_a => 20480,
+		numwords_a => 256,
 		operation_mode => "ROM",
 		outdata_aclr_a => "NONE",
 		outdata_reg_a => "CLOCK0",
-		widthad_a => 15,
+		widthad_a => 8,
 		width_a => 24,
 		width_byteena_a => 1
 	)
@@ -103,41 +103,41 @@ END SYN;
 -- Retrieval info: PRIVATE: JTAG_ENABLED NUMERIC "0"
 -- Retrieval info: PRIVATE: JTAG_ID STRING "NONE"
 -- Retrieval info: PRIVATE: MAXIMUM_DEPTH NUMERIC "0"
--- Retrieval info: PRIVATE: MIFfilename STRING "bongo2.mif"
--- Retrieval info: PRIVATE: NUMWORDS_A NUMERIC "20480"
+-- Retrieval info: PRIVATE: MIFfilename STRING "bongo.mif"
+-- Retrieval info: PRIVATE: NUMWORDS_A NUMERIC "256"
 -- Retrieval info: PRIVATE: RAM_BLOCK_TYPE NUMERIC "0"
 -- Retrieval info: PRIVATE: RegAddr NUMERIC "1"
 -- Retrieval info: PRIVATE: RegOutput NUMERIC "1"
 -- Retrieval info: PRIVATE: SYNTH_WRAPPER_GEN_POSTFIX STRING "0"
 -- Retrieval info: PRIVATE: SingleClock NUMERIC "1"
 -- Retrieval info: PRIVATE: UseDQRAM NUMERIC "0"
--- Retrieval info: PRIVATE: WidthAddr NUMERIC "15"
+-- Retrieval info: PRIVATE: WidthAddr NUMERIC "8"
 -- Retrieval info: PRIVATE: WidthData NUMERIC "24"
 -- Retrieval info: PRIVATE: rden NUMERIC "0"
 -- Retrieval info: LIBRARY: altera_mf altera_mf.altera_mf_components.all
 -- Retrieval info: CONSTANT: ADDRESS_ACLR_A STRING "NONE"
 -- Retrieval info: CONSTANT: CLOCK_ENABLE_INPUT_A STRING "BYPASS"
 -- Retrieval info: CONSTANT: CLOCK_ENABLE_OUTPUT_A STRING "BYPASS"
--- Retrieval info: CONSTANT: INIT_FILE STRING "bongo2.mif"
+-- Retrieval info: CONSTANT: INIT_FILE STRING "bongo.mif"
 -- Retrieval info: CONSTANT: INTENDED_DEVICE_FAMILY STRING "Cyclone III"
 -- Retrieval info: CONSTANT: LPM_HINT STRING "ENABLE_RUNTIME_MOD=NO"
 -- Retrieval info: CONSTANT: LPM_TYPE STRING "altsyncram"
--- Retrieval info: CONSTANT: NUMWORDS_A NUMERIC "20480"
+-- Retrieval info: CONSTANT: NUMWORDS_A NUMERIC "256"
 -- Retrieval info: CONSTANT: OPERATION_MODE STRING "ROM"
 -- Retrieval info: CONSTANT: OUTDATA_ACLR_A STRING "NONE"
 -- Retrieval info: CONSTANT: OUTDATA_REG_A STRING "CLOCK0"
--- Retrieval info: CONSTANT: WIDTHAD_A NUMERIC "15"
+-- Retrieval info: CONSTANT: WIDTHAD_A NUMERIC "8"
 -- Retrieval info: CONSTANT: WIDTH_A NUMERIC "24"
 -- Retrieval info: CONSTANT: WIDTH_BYTEENA_A NUMERIC "1"
--- Retrieval info: USED_PORT: address 0 0 15 0 INPUT NODEFVAL "address[14..0]"
+-- Retrieval info: USED_PORT: address 0 0 8 0 INPUT NODEFVAL "address[7..0]"
 -- Retrieval info: USED_PORT: clock 0 0 0 0 INPUT VCC "clock"
 -- Retrieval info: USED_PORT: q 0 0 24 0 OUTPUT NODEFVAL "q[23..0]"
--- Retrieval info: CONNECT: @address_a 0 0 15 0 address 0 0 15 0
+-- Retrieval info: CONNECT: @address_a 0 0 8 0 address 0 0 8 0
 -- Retrieval info: CONNECT: @clock0 0 0 0 0 clock 0 0 0 0
 -- Retrieval info: CONNECT: q 0 0 24 0 @q_a 0 0 24 0
--- Retrieval info: GEN_FILE: TYPE_NORMAL bongo2.vhd TRUE
--- Retrieval info: GEN_FILE: TYPE_NORMAL bongo2.inc FALSE
--- Retrieval info: GEN_FILE: TYPE_NORMAL bongo2.cmp TRUE
--- Retrieval info: GEN_FILE: TYPE_NORMAL bongo2.bsf FALSE
--- Retrieval info: GEN_FILE: TYPE_NORMAL bongo2_inst.vhd FALSE
+-- Retrieval info: GEN_FILE: TYPE_NORMAL bongo.vhd TRUE
+-- Retrieval info: GEN_FILE: TYPE_NORMAL bongo.inc FALSE
+-- Retrieval info: GEN_FILE: TYPE_NORMAL bongo.cmp TRUE
+-- Retrieval info: GEN_FILE: TYPE_NORMAL bongo.bsf FALSE
+-- Retrieval info: GEN_FILE: TYPE_NORMAL bongo_inst.vhd FALSE
 -- Retrieval info: LIB_FILE: altera_mf
--- practices/40-noel/lcd_pic_test/test.vhd
+++ practices/40-noel/lcd_pic_test/test.vhd
@@ -20,14 +20,14 @@ architecture arch of test is
 	signal p_data_i : std_logic_vector(23 downto 0);
 	signal p_data : l_px_t;
 
-	signal x : Integer range -127 to 127;
-	signal y : Integer range -159 to 159;
+	signal x : integer range -127 to 127;
+	signal y : integer range -159 to 159;
 	signal font_start, font_busy, l_clear : std_logic;
 	signal pic_data_o : l_px_t;
 begin
-	bongo_inst : entity work.bongo2(syn)
+	bongo_inst : entity work.bongo(syn)
 		port map(
-			address => std_logic_vector(to_unsigned(l_addr, 10)),
+			address => std_logic_vector(to_unsigned(l_addr, 8)),
 			clock   => clk,
 			q       => p_data_i
 		);
@@ -46,49 +46,49 @@ begin
 	-- 		addr       => l_addr,
 	-- 		data       => l_data
 	-- 	);
-	lcd_mix_inst: entity work.lcd_mix(arch)
-	port map (
-		clk 			 => clk,
-		rst_n 			 => rst_n,
-		x 				 => x,
-		y 				 => y,
-		font_start 		 => font_start,
-		font_busy 		 => font_busy,
-		text_size 		 => 1,
-		text_data 		 => "text_data   ",
-		text_count 		 => open,
-		addr 			 => open,
-		text_color 		 => green,
-		bg_color 		 => white,
-		text_color_array => (green, green, green, green, green, green, green, green, green, green, green, green),
-		clear 			 => l_clear,
-		lcd_sclk 		 => lcd_sclk,
-		lcd_mosi 		 => lcd_mosi,
-		lcd_ss_n 		 => lcd_ss_n,
-		lcd_dc 			 => lcd_dc,
-		lcd_bl 			 => lcd_bl,
-		lcd_rst_n 		 => lcd_rst_n,
-		con 			 => '1',
-		pic_addr 		 => l_addr,
-		pic_data 		 => pic_data_o
-	);
+	lcd_mix_inst : entity work.lcd_mix(arch)
+		port map(
+			clk              => clk,
+			rst_n            => rst_n,
+			x                => x,
+			y                => y,
+			font_start       => font_start,
+			font_busy        => font_busy,
+			text_size        => 1,
+			text_data        => "text_data   ",
+			text_count       => open,
+			addr             => open,
+			text_color       => green,
+			bg_color         => white,
+			text_color_array => (green, green, green, green, green, green, green, green, green, green, green, green),
+			clear            => l_clear,
+			lcd_sclk         => lcd_sclk,
+			lcd_mosi         => lcd_mosi,
+			lcd_ss_n         => lcd_ss_n,
+			lcd_dc           => lcd_dc,
+			lcd_bl           => lcd_bl,
+			lcd_rst_n        => lcd_rst_n,
+			con              => '1',
+			pic_addr         => l_addr,
+			pic_data         => pic_data_o
+		);
 	p_data <= unsigned(p_data_i);
 	process (clk, rst_n)
 	begin
 		if rst_n = '0' then
 		elsif rising_edge(clk) then
-			-- l_data <= l_paste_txt(l_addr, white, "testtesttest", (50, 30));
-			pic_data_o <= to_data(l_paste(l_addr, white, green, (1, 9), 32, 32));
-			-- if l_addr < l_px_cnt - 1 then
-			-- 	if wr_ena = '0' then
-			-- 		wr_ena <= '1';
+			-- pic_data_o <= p_data;
+			pic_data_o <= to_data(l_paste(l_addr, white, p_data, (0, 0), 16, 16));
+			-- 	if l_addr < l_px_cnt - 1 then
+			-- 		if wr_ena = '0' then
+			-- 			wr_ena <= '1';
+			-- 		else
+			-- 			wr_ena <= '0';
+			-- 			l_addr <= l_addr + 1;
+			-- 		end if;
 			-- 	else
 			-- 		wr_ena <= '0';
-			-- 		l_addr <= l_addr + 1;
 			-- 	end if;
-			-- else
-			-- 	wr_ena <= '0';
-			-- end if;
 		end if;
 	end process;
 end arch;
