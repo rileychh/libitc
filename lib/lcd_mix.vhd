115a71c3f99123b26f76c935e9c931cb46cca4ab
MingHaoTai <nnn122nnn70@gmail.com>
2023-11-10 16:13:19 +0800

    lcd_mix.vhd 修改

--- lib/lcd_mix.vhd
+++ lib/lcd_mix.vhd
@@ -23,8 +23,8 @@ entity lcd_mix is
 		text_color_array : in l_px_arr_t(1 to 12);
 		clear            : in std_logic;
 		con              : in std_logic;
-		pic_addr         : out l_addr_t;
-		pic_data         : in l_px_t;
+		-- pic_addr         : out l_addr_t;
+		pic_data : in l_px_t;
 		-- lcd
 		lcd_sclk, lcd_mosi, lcd_ss_n, lcd_dc, lcd_bl, lcd_rst_n : out std_logic
 	);
@@ -38,7 +38,7 @@ architecture arch of lcd_mix is
 	signal l_data : l_px_t;
 	-- signal wr_ena : std_logic;
 	signal q : std_logic_vector(0 downto 0);
-	type status_t is (idle, draw, clear_screen);
+	type status_t is (idle, draw, clear_screen, draw_picture);
 	signal status : status_t;
 	signal p_count : integer range 0 to 20;
 	signal data_y, data_x : integer range 0 to 20;
@@ -117,101 +117,123 @@ begin
 		elsif rising_edge(clk) then
 			if con_h = '1' or con_l = '1' then
 				l_addr <= 0;
-				pic_addr <= 0;
+				-- pic_addr <= 0;
 			end if;
-			if con = '0' then --文字
-				if character'pos(text_data(count + 1)) = 87 then
-					lcd_x <= 1;
-				else
-					lcd_x <= 0;
-				end if;
-				case status is
-					when idle =>
-						if start_draw = '1' then
-							p_count <= text_data'length;
-							wr_ena <= '1';
-							font_busy <= '1';
-							lcd_x <= x;
-							lcd_y <= y;
-							status <= draw;
-						elsif clear = '1' then
-							wr_ena <= '1';
-							font_busy <= '1';
-							addr <= 0;
-							status <= clear_screen;
-						else
-							wr_ena <= '0';
-							font_busy <= '0';
-						end if;
-					when draw =>
-						if (clear = '1') then
-							status <= clear_screen;
-						elsif (pixel_count_x < text_size) then
-							if pixel_count_y < text_size then
-								pixel_count_y <= pixel_count_y + 1;
-								-- if under_line='1' then
-								-- 	l_addr <= data_x * text_size + pixel_count_x + x + 128 * (data_y+2 * text_size + pixel_count_y + y ) + (count * 11 * text_size);
-								-- else
-								l_addr <= data_x * text_size + pixel_count_x + x + 128 * (data_y * text_size + pixel_count_y + y) + (count * 10 * text_size);
-								-- end if;
-							else
-								pixel_count_y <= 0;
-								pixel_count_x <= pixel_count_x + 1;
-							end if;
-						else
-							pixel_count_x <= 0;
-							if data_x + 1 = 11 then
-								data_x <= 0;
-								if data_y + 1 = 20 then
-									data_y <= 0;
-									if count = text_data'length - 1 then
-										status <= idle;
-										count <= 0;
-										data_x <= 0;
-										data_y <= 0;
-									else
-										count <= count + 1;
-									end if;
-								else
-									data_y <= data_y + 1;
-								end if;
-							else
-								data_x <= data_x + 1;
-							end if;
-							color <= text_color_array(count + 1); -- 依位元改變對應顏色										
-						end if;
-						-- if ((data_x * text_size + text_count + x + (count * 10 * text_size)) > 127) or ((data_y * text_size + text_count + y) > 159) or ((data_y * text_size + text_count + y) < 0) or (data_x * text_size + text_count + x + (count * 10)) < 0 then
-						if ((data_x * text_size + text_count + x + (count * 10 * text_size)) > 150) or ((data_y * text_size + text_count + y) > 159) or ((data_y * text_size + text_count + y) < 0) or (data_x * text_size + text_count + x + (count * 10)) < 0 then
-							wr_ena <= '0';
-						else
-							wr_ena <= '1';
-						end if;
-					when clear_screen =>
-						font_busy <= '1';
-						if addr = addr'high then
-							addr <= 0;
-							font_busy <= '0';
-							status <= idle;
-						else
-							addr <= addr + 1;
-						end if;
-						l_addr <= addr;
-					when others =>
-						status <= idle;
-				end case;
-			elsif con = '1' then --圖片
-				l_addr <= pic_addr;
-				if pic_addr < l_px_cnt - 1 then
-					if wr_ena = '0' then
+			-- if con = '0' then --文字
+			if character'pos(text_data(count + 1)) = 87 then
+				lcd_x <= 1;
+			else
+				lcd_x <= 0;
+			end if;
+			case status is
+				when idle =>
+					if start_draw = '1' and con = '0' and clear = '0' then
+						p_count <= text_data'length;
 						wr_ena <= '1';
+						font_busy <= '1';
+						lcd_x <= x;
+						lcd_y <= y;
+						status <= draw;
+					elsif con = '1' and clear = '0' then
+						wr_ena <= '1';
+						font_busy <= '1';
+						addr <= 0;
+						status <= draw_picture;
+					elsif clear = '1' then
+						wr_ena <= '1';
+						font_busy <= '1';
+						addr <= 0;
+						status <= clear_screen;
 					else
 						wr_ena <= '0';
-						pic_addr <= pic_addr + 1;
+						font_busy <= '0';
 					end if;
-				else
-					wr_ena <= '0';
-				end if;
-			end if;
+				when draw =>
+					if (clear = '1') then
+						status <= clear_screen;
+					elsif (pixel_count_x < text_size) then
+						if pixel_count_y < text_size then
+							pixel_count_y <= pixel_count_y + 1;
+							-- if under_line='1' then
+							-- 	l_addr <= data_x * text_size + pixel_count_x + x + 128 * (data_y+2 * text_size + pixel_count_y + y ) + (count * 11 * text_size);
+							-- else
+							l_addr <= data_x * text_size + pixel_count_x + x + 128 * (data_y * text_size + pixel_count_y + y) + (count * 10 * text_size);
+							-- end if;
+						else
+							pixel_count_y <= 0;
+							pixel_count_x <= pixel_count_x + 1;
+						end if;
+					else
+						pixel_count_x <= 0;
+						if data_x + 1 = 11 then
+							data_x <= 0;
+							if data_y + 1 = 20 then
+								data_y <= 0;
+								if count = text_data'length - 1 then
+									status <= idle;
+									count <= 0;
+									data_x <= 0;
+									data_y <= 0;
+								else
+									count <= count + 1;
+								end if;
+							else
+								data_y <= data_y + 1;
+							end if;
+						else
+							data_x <= data_x + 1;
+						end if;
+						color <= text_color_array(count + 1); -- 依位元改變對應顏色										
+					end if;
+					-- if ((data_x * text_size + text_count + x + (count * 10 * text_size)) > 127) or ((data_y * text_size + text_count + y) > 159) or ((data_y * text_size + text_count + y) < 0) or (data_x * text_size + text_count + x + (count * 10)) < 0 then
+					if ((data_x * text_size + text_count + x + (count * 10 * text_size)) > 150) or ((data_y * text_size + text_count + y) > 159) or ((data_y * text_size + text_count + y) < 0) or (data_x * text_size + text_count + x + (count * 10)) < 0 then
+						wr_ena <= '0';
+					else
+						wr_ena <= '1';
+					end if;
+				when clear_screen =>
+					font_busy <= '1';
+					if addr = addr'high then
+						addr <= 0;
+						font_busy <= '0';
+						status <= idle;
+					else
+						addr <= addr + 1;
+					end if;
+					l_addr <= addr;
+				when draw_picture =>
+					l_addr <= addr;
+					if addr < addr'high then
+						if wr_ena = '0' then
+							wr_ena <= '1';
+						else
+							font_busy <= '1';
+							wr_ena <= '0';
+							addr <= addr + 1;
+						end if;
+					else
+						font_busy <= '0';
+						wr_ena <= '0';
+						status <= idle;
+					end if;
+				when others =>
+					status <= idle;
+			end case;
+			-- elsif con = '1' then --圖片
+			-- 	l_addr <= addr;
+			-- 	if addr < l_px_cnt - 1 then
+			-- 		if wr_ena = '0' then
+			-- 			wr_ena <= '1';
+			-- 		else
+			-- 			font_busy <= '1';
+			-- 			wr_ena <= '0';
+			-- 			addr <= addr + 1;
+			-- 		end if;
+			-- 	else
+			-- 		font_busy <= '0';
+			-- 		wr_ena <= '0';
+			-- 	end if;
+			-- end if;
 		end if;
 	end process;
 	l_addr_p <= 1056 * data_y + data_x + first_px;
